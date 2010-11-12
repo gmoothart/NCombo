4c6e9a0db84ca18de69c73129538dafa783e26cd
Gabe Moothart <gmoothart@gmail.com>
2010-11-12 13:06:36 -0800

    Added unit-test project and web project for live testing.

---
+++ test/ComboHandlerFixture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+
+namespace NComboTest
+{
+    [TestFixture]
+    public class ComboHandlerFixture
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+}
