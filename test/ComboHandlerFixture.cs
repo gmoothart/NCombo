da5440bc7de67828d84bc101ffad5f0f6dfcf598
Gabe Moothart <gmoothart@gmail.com>
2010-11-14 22:34:26 -0800

    First passing test! Added Moq as a dependency

--- test/ComboHandlerFixture.cs
+++ test/ComboHandlerFixture.cs
@@ -4,26 +4,66 @@ using System.Linq;
 using System.Text;
 using MbUnit.Framework;
 using NCombo;
+using System.Web;
+using Moq;
+using System.Collections.Specialized;
 
 namespace NComboTest
 {
     [TestFixture]
     public class ComboHandlerFixture
     {
-        ComboHandler ch;
+        ComboHandler handler;
+        Mock<HttpContextBase> mockContext;
+        Mock<HttpRequestBase> mockRequest;
+        Mock<HttpResponseBase> mockResponse;
+        Mock<HttpServerUtilityBase> mockServer;
+
         
         [SetUp]
         public void Setup()
         {
-            ch = new NCombo.ComboHandler();
+            handler = new NCombo.ComboHandler();
 
+            mockRequest = new Mock<HttpRequestBase>();
+            mockRequest.Setup(r => r.Headers).Returns(new NameValueCollection());
 
+            mockResponse = new Mock<HttpResponseBase>();
+            mockResponse.Setup(r => r.WriteFile(It.IsAny<string>()));
+            mockResponse.Setup(r => r.Write('\n'));
+
+            //
+            // Response.End() interupts execution in a server context. To 
+            // simulate the same thing in our tests, we have it throw.
+            mockResponse.Setup(r => r.End()).Throws(new ApplicationException("all done here"));
+
+            mockServer = new Mock<HttpServerUtilityBase>();
+
+            mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(ctxt => ctxt.Request).Returns(mockRequest.Object);
+            mockContext.Setup(ctxt => ctxt.Response).Returns(mockResponse.Object);
+            mockContext.Setup(ctxt => ctxt.Server).Returns(mockServer.Object);
         }
 
         [Test]
         public void Handler_Throws404_IfAFileIsNotFound()
         {
-            Assert.Fail();
+            mockRequest.Setup(m => m.Url)
+                       .Returns(new Uri("http://app/ncombo.axd?file/not/found.js"));
+            mockServer.Setup(m => m.MapPath("~/yui/file/not/found.js"))
+                      .Returns(@"c:\foo\bar\baz.txt");
+
+            try {
+                handler.HandleRequest(mockContext.Object);
+            }
+            catch (ApplicationException ex)
+            {
+                // If this is a Response.End() exception, we can safely ignore.
+                // Otherwise, thow.
+                if (ex.Message != "all done here") throw;
+            }
+
+            mockResponse.VerifySet(m => m.StatusCode = 404);
         }
 
         [Test]
@@ -31,6 +71,12 @@ namespace NComboTest
         {
             Assert.Fail();
 
+        }
+
+        [Test]
+        public void Handler_Returns200OnSuccess()
+        {
+            Assert.Fail();
         }
 
         [Test]
